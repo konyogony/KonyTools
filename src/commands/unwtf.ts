44f7b21c5c6f21cceb9b0e3e85da99265d9b86ab
kony_ogony <45127834+Kony-Ogony@users.noreply.github.com>
2024-08-12 19:36:13 +0700

    keep caps + rm console log

--- src/commands/unwtf.ts
+++ src/commands/unwtf.ts
@@ -8,50 +8,80 @@ export const options = new ContextMenuCommandBuilder()
 export const run = async (interaction: MessageContextMenuCommandInteraction<'cached'>) => {
     if (interaction.commandType !== ApplicationCommandType.Message) return;
     const wtfmsg = interaction.targetMessage;
-    console.log(interaction.commandType);
-    const unwtf: String[] = [];
+    const unwtf: string[] = [];
     type LanguageMap = {
         [key: string]: string;
     };
     const englishToRussian: LanguageMap = {
         q: 'й',
+        Q: 'Й',
         w: 'ц',
+        W: 'Ц',
         e: 'у',
+        E: 'У',
         r: 'к',
+        R: 'К',
         t: 'е',
+        T: 'Е',
         y: 'н',
+        Y: 'Н',
         u: 'г',
+        U: 'Г',
         i: 'ш',
+        I: 'Ш',
         o: 'щ',
+        O: 'Щ',
         p: 'з',
+        P: 'З',
         '[': 'х',
+        '{': 'Х',
         ']': 'ъ',
+        '}': 'Ъ',
         a: 'ф',
+        A: 'Ф',
         s: 'ы',
+        S: 'Ы',
         d: 'в',
+        D: 'В',
         f: 'а',
+        F: 'А',
         g: 'п',
+        G: 'П',
         h: 'р',
+        H: 'Р',
         j: 'о',
+        J: 'О',
         k: 'л',
+        K: 'Л',
         l: 'д',
+        L: 'Д',
         ';': 'ж',
+        ':': 'Ж',
         "'": 'э',
+        '"': 'Э',
         z: 'я',
+        Z: 'Я',
         x: 'ч',
+        X: 'Ч',
         c: 'с',
+        C: 'С',
         v: 'м',
+        V: 'М',
         b: 'и',
+        B: 'И',
         n: 'т',
+        N: 'Т',
         m: 'ь',
+        M: 'Ь',
         ',': 'б',
+        '<': 'Б',
         '.': 'ю',
+        '>': 'Ю',
         ' ': ' ',
     };
+
     wtfmsg.content.split('').forEach((letter) => {
-        console.log(2);
-        unwtf.push(englishToRussian[letter.toLowerCase()] || letter);
+        unwtf.push(englishToRussian[letter] || letter);
     });
-    console.log(3);
     return await interaction.reply(unwtf.join(''));
 };
