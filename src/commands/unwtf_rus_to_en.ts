c1efaaa3b0eb29f575f5cbc127b2378570a8ff67
kony_ogony <45127834+Kony-Ogony@users.noreply.github.com>
2024-08-15 21:08:57 +0700

    me idiot

--- src/commands/unwtf_rus_to_en.ts
+++ src/commands/unwtf_rus_to_en.ts
@@ -1,7 +1,7 @@
 import { ApplicationCommandType, ContextMenuCommandBuilder, MessageContextMenuCommandInteraction } from 'discord.js';
 
 export const options = new ContextMenuCommandBuilder()
-    .setName('unwtf_en_to_rus')
+    .setName('unwtf_rus_to_en')
     .setType(ApplicationCommandType.Message)
     .toJSON();
 
