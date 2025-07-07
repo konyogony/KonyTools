840a279d94c3b18ad24ebca4c0bb6d982d907ab5
konyogony <me@konyogony.dev>
2025-07-07 21:06:59 +0700

    let flop4ik do it :)

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -10,7 +10,7 @@ export default {
     steam_key: process.env['STEAM_KEY']!,
     kony_id: process.env['KONY_ID']!,
     padow_id: '684472142804549637',
-    flop4ik_id: '684472142804549637',
+    flop4ik_id: '544501814255550485',
     djoh_id: '313465742106296340',
     dan_id: '136187150457765888',
     github_id: '727150742078750740',
