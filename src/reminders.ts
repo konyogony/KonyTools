d09a5b26940f3aef700d25ed8eea07aab1bb9625
PadowYT2 <me@padow.ru>
2026-02-19 20:00:14 +0300

    update reminder process

--- src/reminders.ts
+++ src/reminders.ts
@@ -16,22 +16,29 @@ const due = await database
 
 if (due.length === 0) process.exit();
 
-due.forEach(async (reminder) => {
-    const channel = (await rest.post(Routes.userChannels(), { body: { recipient_id: reminder.user } })) as {
-        id: Snowflake;
-    };
+await Promise.all(
+    due.map(async (reminder) => {
+        const channel = (await rest.post(Routes.userChannels(), {
+            body: {
+                recipient_id: reminder.user,
+            },
+        })) as { id: Snowflake };
 
-    const components = [
-        new TextDisplayBuilder().setContent('## Reminder'),
-        new SeparatorBuilder(),
-        new TextDisplayBuilder().setContent(
-            [`## Time`, `<t:${reminder.time * 60}:f>`, `## Content`, reminder.content].join('\n'),
-        ),
-    ].map((c) => c.toJSON());
+        const components = [
+            new TextDisplayBuilder().setContent('## Reminder'),
+            new SeparatorBuilder(),
+            new TextDisplayBuilder().setContent(
+                [`## Time`, `<t:${reminder.time * 60}:f>`, `## Content`, reminder.content].join('\n'),
+            ),
+        ].map((c) => c.toJSON());
 
-    await rest.post(Routes.channelMessages(channel.id), {
-        body: { components, flags: MessageFlags.IsComponentsV2 },
-    });
+        await rest.post(Routes.channelMessages(channel.id), {
+            body: {
+                components,
+                flags: MessageFlags.IsComponentsV2,
+            },
+        });
 
-    await database.delete(reminders).where(eq(reminders.id, reminder.id));
-});
+        await database.delete(reminders).where(eq(reminders.id, reminder.id));
+    }),
+);
